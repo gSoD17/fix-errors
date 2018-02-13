e766dccee629f0d1c353a782361592fa6d8578a1
Peter Horton <phorton@softstackfactory.org>
2018-02-13 05:53:40 +0000

    code not executing because of conditional is not referencing age in the people array, fixed problem by creating a new variable called person, which is assigned to people[i], which loops through the array, then console logged it in which the array names are referenced using dot notation

--- lesson8.js
+++ lesson8.js
@@ -8,10 +8,9 @@ var people = [
 
 var nameFilter = function() {
     for (var i = 0; i < people.length; i++) {
-        let name = people[i].name;
-        let age = people[i].age;
-        if (age < 50) {
-            console.log("My Name is " + name);
+        let person = people[i];
+        if (person.age < 50) {
+            console.log("My Name is " + person.name);
         }
     }
 };
